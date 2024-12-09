c47961bc6ed169b25a122b7465ee97d0c0256473
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-12-09 17:14:52 -0400

    Change to css selector some cases

--- features/step_definitions/AboutGMOSiteSteps.rb
+++ features/step_definitions/AboutGMOSiteSteps.rb
@@ -9,6 +9,6 @@ When(/^I click the "About the GMO Site" button$/) do
   
 Then('I see the list of technologies used for the site') do |table|
     expected_technologies = table.raw.flatten
-    actual_technologies = page.all(:xpath, '/html/body/div/center/table/tbody/tr[2]/td[1]/blockquote/ul/li').map(&:text)
+    actual_technologies = page.all('body > div > center > table > tbody > tr:nth-child(2) > td:nth-child(1) > blockquote > ul > li').map(&:text)
     expect(actual_technologies).to match_array(expected_technologies)
   end
