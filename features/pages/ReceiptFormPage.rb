0ded8ac5c8a3ffb01a4523d5ff6950a0fadafece
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-12-09 21:28:30 -0400

    Added the Page Object for the ReceiptForm

--- features/pages/ReceiptFormPage.rb
+++ features/pages/ReceiptFormPage.rb
@@ -1,24 +1,34 @@
-class ReceiptPage
-    def initialize
-      @selectors = {
-        'Name' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[1]/td[5]/strong",
-        'Address' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[2]/td[5]/strong",
-        'City' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[3]/td[5]/strong",
-        'Phone' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[4]/td[5]/strong"
-      }
-    end
-  
-    def check_receipt_data(data)
-      full_address = "#{data['City']}, #{data['State']} #{data['Zip']}"
-  
-      @selectors.each do |key, xpath|
-        expected_value = key == 'City' ? full_address : data[key]
-        element = find(:xpath, xpath)
-        expect(element.text.strip).to eq(expected_value)
-      end
-    end
-  
-  
+class ReceiptFormPage
+  include Capybara::DSL
+
+  def initialize
   end
-  
-  
+
+  def validate_receipt(data)
+    full_address = "#{data['City']}, #{data['State']} #{data['Zip']}"
+    selectors = {
+      'Name' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[1]/td[5]/strong",
+      'Address' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[2]/td[5]/strong",
+      'City' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[3]/td[5]/strong",
+      'Phone' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[4]/td[5]/strong"
+    }
+
+    selectors.each do |key, xpath|
+      expected_value = case key
+                       when 'City'
+                         full_address
+                       else
+                         data[key]
+                       end
+
+      element = find(:xpath, xpath)
+      raise "Expected '#{expected_value}', but got '#{element.text.strip}'" unless element.text.strip == expected_value
+    end
+  end
+
+  def validate_successful_notification
+    expected_text = 'Thank you for shopping with Green Mountain Outpost'
+    actual_text = page.find(:xpath, '/html/body/h4/font').text
+    raise "Expected '#{expected_text}', but got '#{actual_text}'" unless actual_text == expected_text
+  end
+end
--- features/step_definitions/ReceiptFormSteps.rb
+++ features/step_definitions/ReceiptFormSteps.rb
@@ -1,36 +1,12 @@
+
+
 Then(/^I see the following data in the receipt$/) do |table|
+  receipt_page = ReceiptFormPage.new
   data = table.rows_hash
-
-  # Store the expected values from the table
-  name = data['Name']
-  address = data['Address']
-  city = data['City']
-  state = data['State']
-  zip = data['Zip']
-  phone = data['Phone']
-  full_address = "#{city}, #{state} #{zip}"
-  selectors = {
-    'Name' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[1]/td[5]/strong",
-    'Address' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[2]/td[5]/strong",
-    'City' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[3]/td[5]/strong",
-    'Phone' => "/html/body/table[2]/tbody/tr[1]/td/div/center/table/tbody/tr[4]/td[5]/strong"
-  }
-
-  selectors.each do |key, xpath|
-    expected_value = case key
-                     when 'City'
-                       full_address 
-                     else
-                       data[key] 
-                     end
-
-    element = find(:xpath, xpath)
-    expect(element.text.strip).to eq(expected_value)
-  end
+  receipt_page.validate_receipt(data)
 end
 
 Then(/^I see the succesful notification$/) do
-  expected_text = 'Thank you for shopping with Green Mountain Outpost'
-  actual_text = page.find(:xpath, '/html/body/h4/font').text
-  expect(actual_text).to eq(expected_text)
+  receipt_page = ReceiptFormPage.new
+  receipt_page.validate_successful_notification
 end
