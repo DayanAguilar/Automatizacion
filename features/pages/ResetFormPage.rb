4b2a71974ff0aaba8f554928005df426bf39ed11
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-12-09 21:17:36 -0400

    Added the Page Object for the ResetForm

---
+++ features/pages/ResetFormPage.rb
@@ -0,0 +1,28 @@
+# features/pages/ResetFormPage.rb
+
+class ResetPage
+    include Capybara::DSL
+    def initialize
+    end
+  
+    def click_reset_button
+      page.click_button('bReset')
+    end
+  
+    def validate_form(table)
+      within('body > form > table > tbody > tr:nth-child(2) > td > div > center > table') do
+        table.hashes.each do |row|
+          product = row['product']
+          expected_value = row['value'].strip  
+          product_row = find('tr', text: product)
+          actual_value = product_row.find('input[type="text"]').value.strip  
+          if expected_value == '0'
+            raise "Expected '0', but got '#{actual_value}'" unless actual_value == '0'
+          else
+            raise "Expected #{expected_value}, but got #{actual_value}" unless actual_value == expected_value
+          end
+        end
+      end
+    end
+  end
+  
--- features/step_definitions/ResetFormSteps.rb
+++ features/step_definitions/ResetFormSteps.rb
@@ -1,15 +1,10 @@
+# features/step_definitions/ResetFormSteps.rb
+
 When(/^I click the Reset button$/) do
-    click_button('bReset')
-  end
-  
-  Then(/^The form looks like the initial table$/) do |table|
-    within('body > form > table > tbody > tr:nth-child(2) > td > div > center > table') do
-      table.hashes.each do |row|
-        product = row['product']
-        input = row['input']
-        product_row = find('tr', text: product)
-        actual_value = product_row.find('input[type="text"]').value
-        actual_value == input
-     end
-    end
-  end
+  @reset_page = ResetPage.new
+  @reset_page.click_reset_button
+end
+
+Then(/^The form looks like the initial table$/) do |table|
+  @reset_page.validate_form(table)
+end
